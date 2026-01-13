1be9ffcd444ccc6f63f80e63db61b5f73d2fb321
Alejandro Piad <apiad@apiad.net>
2026-01-12 20:12:18 -0500

    Bump version to 0.4.1

--- src/purely/__init__.py
+++ src/purely/__init__.py
@@ -2,4 +2,4 @@ from .core import ensure, tap, pipe, Chain, Option, safe, curry
 
 __all__ = ["ensure", "tap", "pipe", "Chain", "Option", "safe", "curry"]
 
-__version__ = "0.1.0"
+__version__ = "0.4.1"
