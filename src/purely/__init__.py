4d272cd7cd780611bbf0ffdf95bfc1a3c0597b44
Alejandro Piad <apiad@apiad.net>
2026-01-16 04:19:36 -0500

    Apply code formatting

--- src/purely/__init__.py
+++ src/purely/__init__.py
@@ -1,6 +1,17 @@
 from .core import ensure, tap, pipe, Chain, Option, safe
 from .curry import curry
+from .di import Registry, depends
 
-__all__ = ["ensure", "tap", "pipe", "Chain", "Option", "safe", "curry"]
+__all__ = [
+    "ensure",
+    "tap",
+    "pipe",
+    "Chain",
+    "Option",
+    "safe",
+    "curry",
+    "Registry",
+    "depends",
+]
 
 __version__ = "0.4.2"
